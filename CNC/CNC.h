7e3aaa04d86a1f226617f14a5ed0fe7ae4e60d59
devart-coder <devart.mail@mail.ru>
2026-03-24 00:25:19 +0300

    modified:   CNC/CNC.h

--- CNC/CNC.h
+++ CNC/CNC.h
@@ -1,7 +1,15 @@
 #ifndef CNC_H
 #define CNC_H
-
+#include <Nano.h>
 namespace CNC{
+    using XDir  = Nano::PinD2;
+    using XStep = Nano::PinD5;
+
+    using YDir  = Nano::PinD3;
+    using YStep = Nano::PinD6;
+
+    using ZDir  = Nano::PinD4;
+    using ZStep = Nano::PinD7;
 
 }
 #endif // CNC_H
